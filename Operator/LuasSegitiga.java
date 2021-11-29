988d1c7e91cbb60b94c8a48559e34b5274f8e543
Citra Febriawirti <83469523+citraFebriawirti@users.noreply.github.com>
2021-11-29 18:48:41 +0700

    Add files via upload

---
+++ Operator/LuasSegitiga.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package luassegitiga;
+
+/**
+ *
+ * @author Citra Febriawirti
+ */
+public class LuasSegitiga {// ini untuk class scope merangkup semua class yang ada di dalamnya
+    public static void main(String[] args) {// ini untuk class scope bagian dari dalam class
+        double alas,tinggi,luas;// mendeklarasikan variable dan tipe data
+        alas = 3;// input alas 3
+        tinggi = 7;// input tinggi 7
+        luas  = 0.5 * alas * tinggi;// proses dari alas dan input
+        System.out.println("Luas Segitiga adalah "+ luas);// output Luas Segitiga adalah 10.5
+    }
+    
+}
